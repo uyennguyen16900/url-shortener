6aab1bfe3b2ce8b87cd38a1406849d3e4b89205a
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-06-25 16:52:31 -0700

    Added pymongo dependencies

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/urlshortener')
+host = os.environ.get('MONGODB_URI', 'mongodb://db:27017/urlshortener')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 urls = db.urls
@@ -13,12 +13,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Show the home page"""
+    """Show the home page."""
     return render_template('index.html')
 
 @app.route('/shorten', methods=['POST'])
 def return_shortened():
-    """Shorten the original url and print it"""
+    """Shorten the original url and print it."""
     url_shortener = URL_shortener()
     original_url = request.form['original_url']
     short_url = url_shortener.shorten_url(original_url)
@@ -36,7 +36,7 @@ def return_shortened():
 
 @app.route('/<short_url>')
 def redirect_to_url(short_url):
-    """Expand the shortened url and redirect back to original url"""
+    """Expand the shortened url and redirect back to original url."""
     link_target = urls.find_one({'shortened_url': short_url})
     if link_target:
         return redirect(link_target["original_url"])
